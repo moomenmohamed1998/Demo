22af7252dd0847ed50144d5240f28b3a3484f970
moomen mohammmed <monm73530@gmail.com>
2025-01-10 20:32:31 +0200

    enum

--- Demo/PhoneBook.cs
+++ Demo/PhoneBook.cs
@@ -55,68 +55,69 @@ namespace Demo
         //special Propety
         //this 
 
-        public long this[string name]
-        {
-            get
-            {
-                if (names is not null && numbers is not null)
-                {
-                    for (int i = 0; i < names.Length; i++)
-                    {
-                        if (name == names[i])
-                            return numbers[i];
-                    }
-                }
-                return -1;
-            }
+        //public long this[string name]
+        //{
+        //    get
+        //    {
+        //        if (names is not null && numbers is not null)
+        //        {
+        //            for (int i = 0; i < names.Length; i++)
+        //            {
+        //                if (name == names[i])
+        //                    return numbers[i];
+        //            }
+        //        }
+        //        return -1;
+        //    }
 
-            set
-            {
-                if (names is not null && numbers is not null)
-                {
-                    for (int i = 0; i < names.Length; i++)
-                    {
-                        if (name == names[i])
-                        {
-                            numbers[i] = value;
-                            return;
-                        }
+        //    set
+        //    {
+        //        if (names is not null && numbers is not null)
+        //        {
+        //            for (int i = 0; i < names.Length; i++)
+        //            {
+        //                if (name == names[i])
+        //                {
+        //                    numbers[i] = value;
+        //                    return;
+        //                }
 
-                    }
-                }
-            }
-        }
+        //            }
+        //        }
+        //    }
+        //}
 
 
-        public string this[int index]
-        {
-            get
-            {
-                return $"{index} :: {names[index]} :: {numbers[index]}";
-            }
-        }
-        public PhoneBook(int _size)
-        {
+        //public string this[int index]
+        //{
+        //    get
+        //    {
+        //        return $"{index} :: {names[index]} :: {numbers[index]}";
+        //    }
+        //}
+        //public PhoneBook(int _size)
+        //{
 
-            size = _size;
-            names = new string[size];
-            numbers = new long[size];
-        }
-        public void Addperson(int postion, string Name, long Num)
-        {
-            if (names is not null && numbers is not null)
-            {
-                if (postion >= 0 && postion < size)
-                {
-                    names[postion] = Name;
-                    numbers[postion] = Num;
-                }
-            }
+        //    size = _size;
+        //    names = new string[size];
+        //    numbers = new long[size];
+        //}
+        //public void Addperson(int postion, string Name, long Num)
+        //{
+        //    if (names is not null && numbers is not null)
+        //    {
+        //        if (postion >= 0 && postion < size)
+        //        {
+        //            names[postion] = Name;
+        //            numbers[postion] = Num;
+        //        }
+        //    }
 
-        }
+        //}
 
 
         #endregion
+
 
 
 
