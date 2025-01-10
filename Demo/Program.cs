3a672362dbaa34dcd00f0e7f260dc309bb796f2e
moomen mohammmed <monm73530@gmail.com>
2025-01-10 19:20:49 +0200

    Phone Book

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region PhoneBook
+
+            PhoneBook Note = new PhoneBook(3);
+            Note.Addperson(0, "Ahmed", 123);
+            Note.Addperson(1, "Amr", 456);
+            Note.Addperson(2, "Aya", 789);
+
+
+            //Console.WriteLine(Note.GetPersonNumber("Amr"));
+
+            //Console.WriteLine(Note.GetPersonNumber("Amr"));
+
+            Note.SetPersonNumber("Amr", 101010);
+            Console.WriteLine(Note[0]);
+            #endregion
         }
     }
 }
